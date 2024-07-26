1ff4982c95a6661bf653a9215fc1df83224fcf37
saya-dev <sametcetinkaya00s@gmail.com>
2024-07-27 00:06:10 +0300

    organise imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
 	import '../app.postcss';
+
+	import { i18n } from '$lib/i18n';
+
 	import { ModeWatcher } from 'mode-watcher';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
-	import { i18n } from '$lib/i18n';
 </script>
 
 <ModeWatcher />

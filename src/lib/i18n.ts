efd7bb5dbe605949fac90fb963ad5e77931092dd
saya-dev <sametcetinkaya00s@gmail.com>
2024-07-27 22:52:11 +0300

    use language negotation

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -2,7 +2,9 @@ import { createI18n } from '@inlang/paraglide-sveltekit';
 import { redirect as sveltekitRedirect } from '@sveltejs/kit';
 import * as runtime from '$paraglide/runtime';
 
-export const i18n = createI18n(runtime);
+export const i18n = createI18n(runtime, {
+	prefixDefaultLanguage: 'always'
+});
 
 export const redirect: typeof sveltekitRedirect = (status, location) => {
 	sveltekitRedirect(status, i18n.resolveRoute(location as string));

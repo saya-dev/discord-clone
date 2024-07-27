0c8fb790327bc629ae11015e86665c27f6b9cd17
saya-dev <sametcetinkaya00s@gmail.com>
2024-07-27 23:21:39 +0300

    landing update & translations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,16 @@
 	import '../app.postcss';
 
 	import { i18n } from '$lib/i18n';
+	import * as m from '$paraglide/messages';
 
 	import { ModeWatcher } from 'mode-watcher';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 </script>
+
+<svelte:head>
+	<meta name="description" content={m.landing_hero_description()} />
+	<meta name="author" content={'saya'} />
+</svelte:head>
 
 <ModeWatcher />
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
+	import * as m from '$paraglide/messages';
+
 	import { Button } from '$components/ui/button';
-	import { ExternalLink, LogIn } from 'lucide-svelte';
+	import { LanguageSwitch } from '$components/ui/language-switch';
+	import { LightSwitch } from '$components/ui/light-switch';
+	import { ExternalLink, LogIn, HandMetal } from 'lucide-svelte';
 </script>
+
+<svelte:head>
+	<title>Disclone</title>
+</svelte:head>
 
 <header
 	class="sticky top-0 z-50 w-full border-b bg-background/95 shadow-sm backdrop-blur supports-[backdrop-filter]:bg-background/60"
 >
-	<div class="container flex h-14 items-center justify-between p-4">
-		<a href="/" class="flex items-center gap-2 font-bold">Disclone</a>
+	<div class="container flex h-16 items-center justify-between p-4">
+		<a href="/" class="flex items-center gap-2 text-xl font-bold"
+			><HandMetal class="size-6" /> Disclone</a
+		>
 		<nav class="hidden gap-5 text-sm font-medium text-foreground/80 md:flex">
 			<a
 				class="flex items-center transition-colors hover:text-primary"
@@ -16,21 +26,46 @@
 			>
 		</nav>
 
-		<div class="flex items-center gap-2">
-			<Button href="/login" class="hidden md:flex"><LogIn class="mr-2 w-4" />Login</Button>
+		<div class="flex gap-2">
+			<LanguageSwitch />
+			<LightSwitch />
+			<Button href="/login"><LogIn class="mr-2 size-4" />Login</Button>
 		</div>
 	</div>
 </header>
 
-<div class="flex min-h-[calc(100vh-4rem)] flex-col">
-	<div class="flex-grow"></div>
-	<footer class="mt-4 border-t border-border text-sm leading-6">
-		<div
-			class="max-w-8xl container flex h-16 items-center justify-between gap-3 text-foreground/80"
-		>
-			<div class="flex items-center gap-2 lg:flex-1">
-				<p>Disclone is a discord clone made by saya. It is not affiliated with discord.</p>
+<div class="flex min-h-[calc(100vh-65px)] flex-col">
+	<div class="flex-grow">
+		<main class="container flex max-w-6xl flex-col items-center">
+			<div class="py-20 sm:py-28 md:py-32">
+				<h1
+					class="text-balance text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl"
+				>
+					{m.landing_hero_title()}
+				</h1>
+				<p class="mx-auto mt-6 max-w-3xl text-balance text-center text-base sm:text-lg">
+					{m.landing_hero_description()}
+				</p>
+				<div class="mt-8 flex items-center justify-center gap-4">
+					<Button href="/register" size="lg"
+						><HandMetal class="mr-2 size-5" /> {m.landing_hero_get_started()}</Button
+					>
+				</div>
 			</div>
-		</div>
+		</main>
+	</div>
+	<footer class="mt-4 flex h-16 items-center justify-center border-t border-border text-sm">
+		<p class="mx-4 text-center text-foreground/80">
+			Disclone is a discord clone made by <a
+				href="https://github.com/saya-dev"
+				target="_blank"
+				class="text-foreground underline underline-offset-4">saya</a
+			>. It is not affiliated with
+			<a
+				href="https://discord.com"
+				target="_blank"
+				class="text-foreground underline underline-offset-4">discord</a
+			>.
+		</p>
 	</footer>
 </div>

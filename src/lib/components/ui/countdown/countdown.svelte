20764d939082fa04b751beedbbcace6f100c6cb2
saya-dev <sametcetinkaya00s@gmail.com>
2024-08-09 22:09:26 +0300

    add countdown

---
+++ src/lib/components/ui/countdown/countdown.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	export let seconds: number;
+
+	const dispatch = createEventDispatcher();
+
+	let time = new Date(seconds * 1000).toISOString().substring(11, 19);
+
+	const interval = setInterval(() => {
+		seconds -= 1;
+		time = new Date(seconds * 1000).toISOString().substring(11, 19);
+		if (seconds == 0) {
+			clearInterval(interval);
+			dispatch('finish');
+		}
+	}, 1000);
+</script>
+
+{time}
